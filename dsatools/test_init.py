c8eda8a592f4e75ed42cd43d5f9239a7b39b16f7
MVRonkin <70448660+MVRonkin@users.noreply.github.com>
2021-05-12 18:10:14 +0500

    small mistakes corrected

---
+++ dsatools/test_init.py
@@ -0,0 +1,16 @@
+from . import generator
+from . import utilits
+from . import operators
+
+from .import decomposition
+from .import spectrum
+from .import roots
+from .import arma
+from .import frequency
+from .import phase
+from .import delay_fmcw
+from .import distances
+
+if __name__ == "__main__":
+    
+    import __init__
